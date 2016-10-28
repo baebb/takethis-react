406e00156d6cea399c1b35ec515378a0db30f1cc
Ross Dyson <ross@soundninja.com>
2016-10-28 01:47:47 -0700

    added user profile img and placeholder

--- src/components/recommend_item.js
+++ src/components/recommend_item.js
@@ -11,7 +11,13 @@ export default (props) => {
         <div className="col-xs-10">
           <p className="list-group-item-text text-muted"><small>{props.brand}</small></p>
           <h4 className="list-group-item-heading">{props.title}</h4>
-          <p className="list-group-item-text"><small>by {props.recommender ? props.recommender.username : 'Anonymous'}</small></p>
+          <p className="list-group-item-text text-muted"><small>by
+            <img
+              className={"user-img-small img-circle" + (!props.recommender ? " anonymous-one" : '')}
+              width="20px" height="20px"
+              src={props.recommender ? props.recommender.photoURL : null}
+            />
+            {props.recommender ? props.recommender.username : 'Anonymous'}</small></p>
           <p className="list-group-item-text font-italic">{props.reason}</p>
           <span className="tag tag-default pull-xs-right">{props.destination}</span>
         </div>
